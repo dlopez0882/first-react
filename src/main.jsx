7622815616bfa9385f2178bb1528abbc12bde754
David Lopez <dlopez0882@gmail.com>
2025-01-30 15:09:28 -0800

    add styling to react elements

--- src/main.jsx
+++ src/main.jsx
@@ -13,13 +13,13 @@ const root = createRoot(document.getElementById("root"))
 
 function Header() {
     return (
-        <header>
-            <img src="../src/assets/react.svg" width="40px" alt="React logo" />
+        <header className="header">
+            <img src="../src/assets/react.svg" className="nav-logo" alt="React logo" />
             <nav>
-                <ul>
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
                 </ul>
             </nav>
         </header>
@@ -33,9 +33,9 @@ function MainContent() {
                 Reason I am excited to learn React
             </h1>
             <ol>
-                <li>item1</li>
-                <li>item2</li>
-                <li>item3</li>
+                <li className="main-list-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li className="main-list-item">itsed do eiusmod tempor incididunt ut labore et dolore magna aliquaem2</li>
+                <li className="main-list-item">Ut enim ad minim veniam</li>
             </ol>
         </main>
     )
