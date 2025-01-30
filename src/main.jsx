2b106933774533aef8303455ede10dbae75314ba
David Lopez <dlopez0882@gmail.com>
2025-01-30 14:12:00 -0800

    parent/child component challenge

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const root = createRoot(document.getElementById("root"))
+
+function Header() {
+    return (
+        <header>
+            <img src="../src/assets/react.svg" width="40%" alt="React logo" />
+        </header>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <h1>
+                Reason I am excited to learn React
+            </h1>
+            <main>
+                <ol>
+                    <li>item1</li>
+                    <li>item2</li>
+                    <li>item3</li>
+                </ol>
+            </main>
+        </>
+    )
+}
+
+root.render(
+    <Page />
 )
